f5140a6b65db72f68782de5dc80ad6de6bff6902
didrlgus <rlgusdid@naver.com>
2021-09-10 13:55:52 +0900

    [1.1] 초난감 DAO

---
+++ src/main/java/com/example/Application.java
@@ -0,0 +1,32 @@
+package com.example;
+
+import com.example.dao.UserDao;
+import com.example.domain.User;
+import lombok.extern.slf4j.Slf4j;
+
+import java.sql.SQLException;
+
+@Slf4j
+public class Application {
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+
+        UserDao dao = new UserDao();
+
+        User user = new User();
+        user.setId("wade");
+        user.setName("yang");
+        user.setPassword("abcd1234");
+
+        dao.add(user);
+
+        System.out.println(user.getId() + "등록 성공");
+
+        User user2 = dao.get(user.getId());
+
+        System.out.println("name: " + user2.getName());
+        System.out.println("password: " + user2.getPassword());
+        System.out.println(user.getId() + "조회 성공");
+    }
+
+}
---
+++ src/main/java/com/example/domain/User.java
@@ -0,0 +1,15 @@
+package com.example.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class User {
+
+    String id;
+
+    String name;
+
+    String password;
+}
