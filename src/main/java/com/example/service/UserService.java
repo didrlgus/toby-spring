5ca2388572ed790bf5916ddb7c17dabe052feae3
didrlgus <rlgusdid@naver.com>
2021-09-26 17:50:56 +0900

    [5.2.4] 트랜잭션 서비스 추상화

--- src/main/java/com/example/service/UserService.java
+++ src/main/java/com/example/service/UserService.java
@@ -4,31 +4,57 @@ import com.example.dao.UserDao;
 import com.example.domain.Level;
 import com.example.domain.User;
 import java.util.List;
+import javax.sql.DataSource;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 public class UserService {
 
     UserDao userDao;
+    DataSource dataSource;
 
     public static final int MIN_LOG_COUNT_FOR_SILVER = 50;
     public static final int MIN_RECOMMEND_FOR_GOLD = 30;
 
-    public UserService(UserDao userDao) {
+    public UserService(UserDao userDao, DataSource dataSource) {
         this.userDao = userDao;
+        this.dataSource = dataSource;
+    }
+
+    public UserService() {}
+
+    public void setUserDao(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
     }
 
     public void upgradeLevels() {
 
-        List<User> users = userDao.getAll();
+        PlatformTransactionManager transactionManager = new DataSourceTransactionManager(dataSource);
 
-        users.forEach(user -> {
-            if (canUpgradeLevel(user)) {
-                upgradeLevel(user);
-            }
-        });
+        TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
 
+        try {
+            List<User> users = userDao.getAll();
+
+            users.forEach(user -> {
+                if (canUpgradeLevel(user)) {
+                    upgradeLevel(user);
+                }
+            });
+            transactionManager.commit(status);
+        } catch (RuntimeException e) {
+            transactionManager.rollback(status);
+            throw e;
+        }
     }
 
-    private void upgradeLevel(User user) {
+    protected void upgradeLevel(User user) {
         user.upgradeLevel();
         userDao.update(user);
     }
--- src/test/java/com/example/service/UserServiceTest.java
+++ src/test/java/com/example/service/UserServiceTest.java
@@ -3,14 +3,15 @@ package com.example.service;
 import static com.example.service.UserService.MIN_LOG_COUNT_FOR_SILVER;
 import static com.example.service.UserService.MIN_RECOMMEND_FOR_GOLD;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import com.example.configuration.DaoFactory;
 import com.example.dao.UserDao;
 import com.example.domain.Level;
 import com.example.domain.User;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import javax.sql.DataSource;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
@@ -20,6 +21,8 @@ public class UserServiceTest {
 
     private UserService userService;
     private UserDao userDao;
+    private DataSource dataSource;
+
     List<User> users;
 
     @BeforeEach
@@ -29,13 +32,13 @@ public class UserServiceTest {
 
         this.userService = context.getBean("userService", UserService.class);
 
-        users = Arrays.asList(
-            new User("wade", "yang", "p1", Level.BASIC, MIN_LOG_COUNT_FOR_SILVER - 1, 0),
-            new User("joe", "kim", "p2", Level.BASIC, MIN_LOG_COUNT_FOR_SILVER, 0),
-            new User("thomas", "lee", "p3", Level.SILVER, 60, MIN_RECOMMEND_FOR_GOLD - 1),
-            new User("henry", "jang", "p4", Level.SILVER, 60, MIN_RECOMMEND_FOR_GOLD),
-            new User("ron", "oh", "p5", Level.GOLD, 100, Integer.MAX_VALUE)
-        );
+        users = new ArrayList<>();
+        users.add(new User("wade", "yang", "p1", Level.BASIC, MIN_LOG_COUNT_FOR_SILVER - 1, 0));
+        users.add(new User("joe", "kim", "p2", Level.BASIC, MIN_LOG_COUNT_FOR_SILVER, 0));
+        users.add(new User("thomas", "lee", "p3", Level.SILVER, 60, MIN_RECOMMEND_FOR_GOLD - 1));
+        users.add(new User("henry", "jang", "p4", Level.SILVER, 60, MIN_RECOMMEND_FOR_GOLD));
+        users.add(new User("ron", "oh", "p5", Level.GOLD, 100, Integer.MAX_VALUE));
+
         userDao = userService.userDao;
         userDao.deleteAll();
     }
@@ -54,6 +57,25 @@ public class UserServiceTest {
         checkLevelUpgraded(users.get(4), false);
     }
 
+    @Test
+    void upgradeAllOrNothing() {
+        TestUserService testUserService = new TestUserService(users.get(3).getId());
+        testUserService.setUserDao(userDao);
+        testUserService.setDataSource(userService.dataSource);
+
+        for (User user : users) {
+            userDao.add(user);
+        }
+
+        try {
+            testUserService.upgradeLevels();
+            fail("TestUserServiceException expected");
+        } catch (TestUserServiceException e) {
+        }
+
+        checkLevelUpgraded(users.get(1), false);
+    }
+
     private void checkLevelUpgraded(User user, boolean upgraded) {
         User userUpdate = userDao.get(user.getId());
         if (upgraded) {
@@ -66,6 +88,30 @@ public class UserServiceTest {
     private void checkLevel(User user, Level expectedLevel) {
         User userUpdate = userDao.get(user.getId());
         assertThat(userUpdate.getLevel()).isEqualTo(expectedLevel);
+    }
+
+    static class TestUserService extends UserService {
+
+        private String id;
+
+        private TestUserService(String id) {
+            this.id = id;
+        }
+
+        @Override
+        protected void upgradeLevel(User user) {
+            if (user.getId().equals(this.id)) {
+                throw new TestUserServiceException("테스트 유저 서비스 예외 발생!");
+            }
+            super.upgradeLevel(user);
+        }
+    }
+
+    static class TestUserServiceException extends RuntimeException {
+
+        public TestUserServiceException(String message) {
+            super(message);
+        }
     }
 
 }
